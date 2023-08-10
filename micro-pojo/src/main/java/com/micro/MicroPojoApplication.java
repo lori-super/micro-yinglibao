ebc80a51f1eab745438dd4478d3caf4ded2bb955
rechao <448366565@qq.com>
2023-08-10 15:41:51 +0800

    完善index页面功能，完善投资排行榜

---
+++ micro-pojo/src/main/java/com/micro/MicroPojoApplication.java
@@ -0,0 +1,13 @@
+package com.micro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroPojoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroPojoApplication.class, args);
+    }
+
+}
